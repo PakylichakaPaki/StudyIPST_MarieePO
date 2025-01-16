ee31d4bc633b2fb99a981eb10524fdbe89dfe761
MarieePO <pogarskaam@gmail.com>
2025-01-17 00:33:17 +0700

    изменения в day-3

--- day-tree/task-1/index.ts
+++ day-tree/task-1/index.ts
@@ -1,18 +1,26 @@
 function delayPromiseFunction(delay: number, message: string): Promise<string> {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(message);
-        }, delay);
-    });
-  }
-  
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          resolve(message);
+      }, delay);
+  });
+}
+
 // Пример использования
-  async function runDelays() {
-    try {
-        const message = await delayPromiseFunction(2000, "2 секунды!");
-        console.log(message);
-    } catch (error) {
-        console.error("Произошла ошибка:", error);
-    }
+async function runDelays() {
+  try {
+      const message = await delayPromiseFunction(2000, "2 секунды!");
+      return message; // вместо console.log
+  } catch (error) {
+      throw new Error(`Произошла ошибка: ${error}`); // вместо console.error
   }
-  runDelays();
+}
+
+(async () => {
+  try {
+      const result = await runDelays();
+      console.log(result); 
+  } catch (error) {
+      console.error(error.message);
+  }
+})();
